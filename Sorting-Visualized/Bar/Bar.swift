4fcdbae03c94e2fa41661a534cd118ac72d3a6e6
Marshall <gkslql456@gmail.com>
2020-04-21 21:45:51 -0700

    added button conditions and bubble sorting visualization using timer

--- Sorting-Visualized/Bar/Bar.swift
+++ Sorting-Visualized/Bar/Bar.swift
@@ -3,13 +3,14 @@ import Foundation
 
 struct Bar: View, Hashable {
     var value: CGFloat
+    var selected: Bool
     
     var body: some View {
         ZStack(alignment: .bottom) {
             Capsule().frame(width: BarWidth, height: BarMaxHeight)
                 .foregroundColor(Color(#colorLiteral(red: 0, green: 0.8890399337, blue: 0.7886388898, alpha: 1)))
             Capsule().frame(width: BarWidth, height: value)
-                .foregroundColor(Color.white)
+                .foregroundColor(selected ? Color(#colorLiteral(red: 0.9568627477, green: 0.6588235497, blue: 0.5450980663, alpha: 1)) : .white)
         }.padding(.top, 40)
     }
 }
--- Sorting-Visualized/ContentView.swift
+++ Sorting-Visualized/ContentView.swift
@@ -11,6 +11,13 @@ import SwiftUI
 let BarWidth: CGFloat = 10
 let BarMaxHeight: CGFloat = 200
 let BarUnit: CGFloat = 10
+let BarColor_Default: Color = .white
+let BarColr_Selected: Color = .purple
+
+let SortingSpeed: TimeInterval = 0.4
+
+var isRunning = false
+var timer: Timer?
 
 struct ContentView: View {
     @State var pickerSelected = 0
@@ -44,7 +51,9 @@ struct ContentView: View {
                 
                 HStack(spacing: 30) {
                     Button(action: {
-                        self._startSorting()
+                        if !isRunning {
+                            self._startSorting()
+                        }
                     }) {
                         Text("Start")
                             .fontWeight(.heavy)
@@ -53,7 +62,10 @@ struct ContentView: View {
                     }
                     
                     Button(action: {
-                        self._shuffleList()
+                        // list shoudn't be shuffled while soring
+                        if !isRunning {
+                            self._shuffleList()
+                        }
                     }) {
                         Text("Shuffle")
                             .fontWeight(.heavy)
@@ -70,7 +82,7 @@ struct ContentView: View {
     // This performs sorting algorithms, and adjust bar's value to be rendered
     public func _startSorting() -> Void {
         switch pickerSelected {
-            case 0: _bubbleSort()
+            case 0: _performSorting()
             case 1: _bubbleSort()
             case 2: _bubbleSort()
             case 3: _bubbleSort()
@@ -89,22 +101,27 @@ struct ContentView: View {
     
     
     //========================= SORTINGS =========================
+    public func _performSorting() {
+        isRunning = true
+        timer = Timer.scheduledTimer(withTimeInterval: SortingSpeed, repeats: true) { timer in
+            self._bubbleSort()
+        }
+    }
+    
     // perform bubble sort
-    public func _bubbleSort() -> Void{
+    public func _bubbleSort() -> Void {
         let length: Int = self.barList.count
         for i in 0..<length - 1 {
             for j in 0..<length - i - 1 {
-                if barList[j].value > barList[j + 1].value {
-                    DispatchQueue.main.async {
-                        self._swap(this: j, that: j + 1);
-                        print("swap")
-                    }
-                    DispatchQueue.global(qos: .userInteractive).async {
-                        print("sleep")
-                    }
+                self.barList[j].selected = true
+                if self.barList[j].value > self.barList[j + 1].value {
+                    self._swap(this: j, that: j + 1);
+                    return; // timer loop goes on as long as it's valid
                 }
             }
         }
+        timer?.invalidate() // invalidate timer loop
+        isRunning = false // update running status once sorting is finished
     }
     
     // perform selection sort
@@ -136,9 +153,8 @@ struct ContentView: View {
         self.barList[this] = self.barList[that]
         self.barList[that] = temp
     }
-    
-    //============================================================
 
+    //============================================================
 }
 
 
@@ -151,7 +167,7 @@ private func _initializeBarList() -> [Bar] {
     // starting from 1 to multiply
     for n in 1...Int((UIScreen.main.bounds.width - 10) / (BarWidth + 10)) {
         let input: CGFloat = (CGFloat(n) * BarUnit)
-        out.append(Bar(value: input))
+        out.append(Bar(value: input, selected: false))
         if input >= BarMaxHeight { // shouldn't exceed the max height. stop adding
             break
         }
